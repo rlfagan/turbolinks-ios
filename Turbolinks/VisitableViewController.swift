c4c50b2ae7db370e2b73490b96c8d08d4dd7afa8
Aydan Bedingham <aydan.bedingham@pwc.com>
2017-05-29 14:37:16 +1000

    Updated Turbolinks code to Swift3 (Turbolinks v2.0.0)

--- Turbolinks/VisitableViewController.swift
+++ Turbolinks/VisitableViewController.swift
@@ -1,50 +1,62 @@
 import UIKit
 
-public class VisitableViewController: UIViewController, Visitable {
-    public weak var visitableDelegate: VisitableDelegate?
+open class VisitableViewController: UIViewController, Visitable {
+    open weak var visitableDelegate: VisitableDelegate?
 
-    public var visitableURL: NSURL!
+    open var visitableURL: URL!
 
-    public convenience init(URL: NSURL) {
+    public convenience init(url: URL) {
         self.init()
-        self.visitableURL = URL
+        self.visitableURL = url
     }
 
     // MARK: Visitable View
 
-    public lazy var visitableView: VisitableView! = {
-        let view = VisitableView(frame: CGRectZero)
+    open private(set) lazy var visitableView: VisitableView! = {
+        let view = VisitableView(frame: CGRect.zero)
         view.translatesAutoresizingMaskIntoConstraints = false
         return view
     }()
 
-    private func installVisitableView() {
+    fileprivate func installVisitableView() {
         view.addSubview(visitableView)
-        view.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("H:|[view]|", options: [], metrics: nil, views: [ "view": visitableView ]))
-        view.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("V:|[view]|", options: [], metrics: nil, views: [ "view": visitableView ]))
+        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|[view]|", options: [], metrics: nil, views: [ "view": visitableView ]))
+        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[view]|", options: [], metrics: nil, views: [ "view": visitableView ]))
     }
 
     // MARK: Visitable
 
-    public func visitableDidRender() {
+    open func visitableDidRender() {
         self.title = visitableView.webView?.title
     }
 
     // MARK: View Lifecycle
 
-    public override func viewDidLoad() {
+    open override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = UIColor.whiteColor()
+        view.backgroundColor = UIColor.white
         installVisitableView()
     }
 
-    public override func viewWillAppear(animated: Bool) {
+    open override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         visitableDelegate?.visitableViewWillAppear(self)
     }
 
-    public override func viewDidAppear(animated: Bool) {
+    open override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         visitableDelegate?.visitableViewDidAppear(self)
     }
+
+    /*
+     If the visitableView is a child of the main view, and anchored to its top and bottom, then it's
+     unlikely you will need to customize the layout. But more complicated view hierarchies and layout 
+     may require explicit control over the contentInset. Below is an example of setting the contentInset 
+     to the layout guides.
+     
+    public override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        visitableView.contentInset = UIEdgeInsets(top: topLayoutGuide.length, left: 0, bottom: bottomLayoutGuide.length, right: 0)
+    }
+    */
 }
